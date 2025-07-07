a253c7a934b92e816d9ac208542931f811b1b415
Alexis Banda <cbanda@summasolutions.net>
2025-07-07 12:25:31 -0700

    feat: icon fixes refs:#master

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,90 +1,239 @@
 import React from 'react';
+// Importa la configuración de la librería principal de Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+// Importa los iconos sólidos que vas a usar
+import {
+    faBriefcase,
+    faUserCircle,
+    faMapPin,
+    faPlusCircle,
+    faBell,
+    faListUl, // Equivalente a ListBulletIcon
+    faChevronLeft,
+    faChevronRight,
+    faCalendarDays,
+    faLock, // Equivalente a LockClosedIcon
+    faUsers, // Equivalente a UserGroupIcon
+    faEnvelope,
+    faLink,
+    faUtensils, // Equivalente a FoodIcon
+    faBalanceScale, // Equivalente a LegalIcon
+    faMicrochip, // Equivalente a TechIcon
+    faPalette, // Equivalente a BeautyIcon (usando paleta de colores como metáfora de belleza)
+    faTruck, // Equivalente a DeliveryIcon
+    faGraduationCap, // Equivalente a AcademicCapIcon
+    faCalendarCheck, // Reemplazo para faCalendarStar (para EventIcon)
+    faHome,
+    faEllipsisH, // Equivalente a OtherIcon (tres puntos horizontales)
+    faExclamationCircle,
+    faSearch, // Equivalente a MagnifyingGlassIcon
+    faSignOutAlt, // Equivalente a ArrowLeftOnRectangleIcon
+    faArrowDown,
+    faComments, // Equivalente a ChatBubbleLeftRightIcon (múltiples burbujas de chat)
+    faTimes, // Equivalente a XMarkIcon (una X simple)
+    faDollarSign, // Equivalente a CurrencyDollarIcon
+    faNewspaper,
+    faScaleBalanced, // Otro posible para LegalIcon, más directo
+    faCheckCircle,
+    faInfoCircle,
+    faTimesCircle, // Otro posible para XCircleIcon
+    faCommentDots, // Equivalente a ChatBubbleOvalLeftEllipsisIcon (burbuja de chat con puntos)
+    faPaperPlane // Equivalente a PaperAirplaneIcon
+} from '@fortawesome/free-solid-svg-icons';
+// Importa el componente FontAwesomeIcon para usar los iconos en React
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// Tipo genérico para las props de los iconos SVG
-type IconProps = React.SVGProps<SVGSVGElement>;
+// Añade los iconos a la librería de Font Awesome para que estén disponibles
+library.add(
+    faBriefcase,
+    faUserCircle,
+    faMapPin,
+    faPlusCircle,
+    faBell,
+    faListUl,
+    faChevronLeft,
+    faChevronRight,
+    faCalendarDays,
+    faLock,
+    faUsers,
+    faEnvelope,
+    faLink,
+    faUtensils,
+    faBalanceScale,
+    faMicrochip,
+    faPalette,
+    faTruck,
+    faGraduationCap,
+    faCalendarCheck, // Actualizado aquí
+    faHome,
+    faEllipsisH,
+    faExclamationCircle,
+    faSearch,
+    faSignOutAlt,
+    faArrowDown,
+    faComments,
+    faTimes,
+    faDollarSign,
+    faNewspaper,
+    faScaleBalanced,
+    faCheckCircle,
+    faInfoCircle,
+    faTimesCircle,
+    faCommentDots,
+    faPaperPlane
+);
 
+// Tipo genérico para las props de los iconos, ahora más simple ya que Font Awesome maneja la mayoría
+// Puedes pasar props adicionales que Font AwesomeIcon acepte, como 'size', 'color', 'className'
+type IconProps = {
+    className?: string; // Para aplicar estilos con Tailwind u otros CSS
+    size?: "xs" | "lg" | "sm" | "1x" | "2x" | "3x" | "4x" | "5x" | "6x" | "7x" | "8x" | "9x" | "10x";
+    color?: string;
+    // Agrega cualquier otra prop que necesites pasar a FontAwesomeIcon
+};
+
+// Ahora, cada componente de icono simplemente renderiza un FontAwesomeIcon
+// Mapeamos tus nombres de componentes a los nombres de iconos de Font Awesome
 export const BriefcaseIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.075c0 1.313-.964 2.505-2.29 2.75-1.09.19-2.213.34-3.355.45-1.142.11-2.293.165-3.46.165s-2.318-.055-3.46-.165c-1.142-.11-2.265-.26-3.355-.45-1.326-.245-2.29-1.437-2.29-2.75V14.15M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H9.75A2.25 2.25 0 007.5 6v2.25m9 0H7.5" />
-    </svg>
+    <FontAwesomeIcon icon={faBriefcase} {...props} />
 );
 
 export const UserCircleIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-    </svg>
+    <FontAwesomeIcon icon={faUserCircle} {...props} />
 );
 
 export const MapPinIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-    </svg>
+    <FontAwesomeIcon icon={faMapPin} {...props} />
 );
 
 export const PlusCircleIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-    </svg>
+    <FontAwesomeIcon icon={faPlusCircle} {...props} />
 );
 
-// --- NUEVOS ICONOS ---
-
 export const BellIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-    </svg>
+    <FontAwesomeIcon icon={faBell} {...props} />
 );
 
 export const ListBulletIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12M8.25 17.25h12M3.75 6.75h.007v.008H3.75V6.75zm0 5.25h.007v.008H3.75v-.008zm0 5.25h.007v.008H3.75v-.008z" />
-    </svg>
+    <FontAwesomeIcon icon={faListUl} {...props} />
 );
 
 export const ChevronLeftIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-    </svg>
+    <FontAwesomeIcon icon={faChevronLeft} {...props} />
 );
 
 export const ChevronRightIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-    </svg>
+    <FontAwesomeIcon icon={faChevronRight} {...props} />
 );
 
-export const CalendarDaysIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-3.75h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-    </svg>
+export const CalendarDaysIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faCalendarDays} {...props} />
 );
 
 export const LockClosedIcon: React.FC<IconProps> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 00-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-    </svg>
+    <FontAwesomeIcon icon={faLock} {...props} />
 );
 
-export const UserGroupIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth={1.8}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
-    >
-        {/* Persona central */}
-        <circle cx="12" cy="9" r="3" />
-        {/* Personas laterales (cabezas) */}
-        <circle cx="6.5" cy="11.5" r="2" />
-        <circle cx="17.5" cy="11.5" r="2" />
-        {/* Cuerpos */}
-        <path d="M9 21v-2a3 3 0 0 1 6 0v2" />
-        <path d="M4 19v-1a2 2 0 0 1 3-1.732" />
-        <path d="M20 19v-1a2 2 0 0 0-3-1.732" />
-    </svg>
+export const UserGroupIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faUsers} {...props} />
+);
+
+export const EnvelopeIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faEnvelope} {...props} />
+);
+
+export const LinkIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faLink} {...props} />
+);
+
+export const FoodIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faUtensils} {...props} />
+);
+
+export const LegalIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faBalanceScale} {...props} />
+);
+
+export const TechIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faMicrochip} {...props} />
+);
+
+export const BeautyIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faPalette} {...props} />
+);
+
+export const DeliveryIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faTruck} {...props} />
+);
+
+export const AcademicCapIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faGraduationCap} {...props} />
+);
+
+export const EventIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faCalendarCheck} {...props} />
+);
+
+export const HomeIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faHome} {...props} />
+);
+
+export const OtherIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faEllipsisH} {...props} />
+);
+
+export const ExclamationCircleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faExclamationCircle} {...props} />
+);
+
+export const MagnifyingGlassIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faSearch} {...props} />
+);
+
+export const ArrowLeftOnRectangleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faSignOutAlt} {...props} />
+);
+
+export const ArrowDownIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faArrowDown} {...props} />
+);
+
+export const ChatBubbleLeftRightIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faComments} {...props} />
+);
+
+export const XMarkIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faTimes} {...props} />
+);
+
+export const CurrencyDollarIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faDollarSign} {...props} />
+);
+
+export const NewspaperIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faNewspaper} {...props} />
+);
+
+export const ScaleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faScaleBalanced} {...props} />
+);
+
+export const CheckCircleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faCheckCircle} {...props} />
+);
+
+export const InformationCircleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faInfoCircle} {...props} />
+);
+
+export const XCircleIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faTimesCircle} {...props} />
+);
+
+export const ChatBubbleOvalLeftEllipsisIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faCommentDots} {...props} />
+);
+
+export const PaperAirplaneIcon: React.FC<IconProps> = (props) => (
+    <FontAwesomeIcon icon={faPaperPlane} {...props} />
 );
